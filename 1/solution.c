97ba95c673f81b0f365e145c4d9fe102fcedbb56
Dirakon <609700zxc@mail.ru>
2023-06-15 00:19:08 +0300

    Week 1 solution

--- 1/solution.c
+++ 1/solution.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "libcoro.h"
 
 /**
@@ -10,18 +11,145 @@
  * $> ./a.out
  */
 
-/**
- * A function, called from inside of coroutines recursively. Just to demonstrate
- * the example. You can split your code into multiple functions, that usually
- * helps to keep the individual code blocks simple.
- */
-static void
-other_function(const char *name, int depth)
-{
-	printf("%s: entered function, depth = %d\n", name, depth);
-	coro_yield();
-	if (depth < 3)
-		other_function(name, depth + 1);
+struct single_sorted_file_data {
+    int number_count;
+    int *sorted_numbers;
+};
+
+void coro_yield_with_respect_to_quantum(struct timespec *coro_start, int quantum_limit) {
+    struct timespec now;
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    long time_spent_this_run = now.tv_nsec - coro_start->tv_nsec;
+    if (time_spent_this_run >= quantum_limit) {
+        coro_yield();
+        clock_gettime(CLOCK_MONOTONIC, coro_start);
+    }
+}
+
+int quick_sort_partition(int number_count, int *numbers, struct timespec *coro_start, int quantum_limit) {
+    int pivot_value = numbers[number_count - 1];
+    int pivot_index = 0;
+    for (int i = 0; i < number_count; ++i) {
+        if (numbers[i] <= pivot_value) {
+            int temp = numbers[i];
+            numbers[i] = numbers[pivot_index];
+            numbers[pivot_index] = temp;
+            pivot_index++;
+        }
+        coro_yield_with_respect_to_quantum(coro_start, quantum_limit);
+    }
+    return pivot_index - 1;
+
+}
+
+int *get_sorted_inplace_numbers(int number_count, int *numbers, struct timespec *coro_start, int quantum_limit) {
+    if (number_count <= 1)
+        return numbers;
+    int pivot_index = quick_sort_partition(number_count, numbers, coro_start, quantum_limit);
+    get_sorted_inplace_numbers(pivot_index, numbers, coro_start, quantum_limit);
+    get_sorted_inplace_numbers(number_count - pivot_index, numbers + pivot_index, coro_start, quantum_limit);
+
+    return numbers;
+}
+
+struct single_sorted_file_data *
+get_sorted_inplace_file_data(int number_count, int *numbers, struct timespec *coro_start, int quantum_limit) {
+    struct single_sorted_file_data *data = malloc(sizeof(struct single_sorted_file_data));
+
+    data->number_count = number_count;
+    data->sorted_numbers = get_sorted_inplace_numbers(number_count, numbers, coro_start, quantum_limit);
+
+    return data;
+}
+
+void dispose_of_sorted_file_data(struct single_sorted_file_data *data) {
+    free(data->sorted_numbers);
+    free(data);
+}
+
+struct file_queue {
+    int file_ptr;
+    int file_count;
+    char **file_names;
+    struct single_sorted_file_data **sorted_files;
+};
+
+
+struct file_queue *create_file_queue(int file_count, char **file_names) {
+    struct file_queue *queue = malloc(sizeof(struct file_queue));
+
+    queue->file_count = file_count;
+    queue->file_ptr = 0;
+    queue->file_names = malloc(sizeof(char *) * file_count);
+    queue->sorted_files = malloc(sizeof(struct single_sorted_file_data) * file_count);
+    for (int i = 0; i < file_count; ++i) {
+        queue->file_names[i] = file_names[i];
+        queue->sorted_files[i] = NULL;
+    }
+
+    return queue;
+}
+
+void dispose_of_file_queue(struct file_queue *queue) {
+    // We do not dispose of individual file names as they come from argv and are deallocated for us
+    free(queue->file_names);
+    for (int file_ptr = 0; file_ptr < queue->file_count; ++file_ptr) {
+        dispose_of_sorted_file_data(queue->sorted_files[file_ptr]);
+    }
+    free(queue->sorted_files);
+    free(queue);
+}
+
+
+struct coroutine_data {
+    int quantum;
+    char *name;
+    struct file_queue *shared_file_queue;
+    struct timespec start_timespec;
+};
+
+
+struct coroutine_data *create_coroutine_data(char *name, int quantum, struct file_queue *shared_file_queue) {
+    struct coroutine_data *data = malloc(sizeof(struct coroutine_data));
+
+    data->name = name;
+    data->quantum = quantum;
+    data->shared_file_queue = shared_file_queue;
+
+    return data;
+}
+
+void dispose_of_coroutine_data(struct coroutine_data *data) {
+    free(data->name);
+    free(data);
+}
+
+
+int count_numbers_in_file(char *file_name) {
+    FILE *fp = fopen(file_name, "r");
+    int numbers = 0;
+
+    int dummy_number;
+
+    while (fscanf(fp, "%d", &dummy_number) == 1) {
+        numbers++;
+    }
+
+    // after being done with fp
+    fclose(fp);
+    return numbers;
+}
+
+int *get_numbers_in_file(char *file_name, int number_count) {
+    FILE *fp = fopen(file_name, "r");
+    int number_ptr = 0;
+    int *array = malloc(sizeof(int) * number_count);
+
+    while (fscanf(fp, "%d", &array[number_ptr++]) == 1);
+
+    // after being done with fp
+    fclose(fp);
+    return array;
 }
 
 /**
@@ -29,67 +157,105 @@ other_function(const char *name, int depth)
  * implement your solution, sort each individual file.
  */
 static int
-coroutine_func_f(void *context)
-{
-	/* IMPLEMENT SORTING OF INDIVIDUAL FILES HERE. */
+coroutine_func_f(void *coroutine_data) {
+    struct coro *this = coro_this();
+    struct coroutine_data *data = coroutine_data;
+    printf("coroutine %s starts\n", data->name);
+    clock_gettime(CLOCK_MONOTONIC, &data->start_timespec);
 
-	struct coro *this = coro_this();
-	char *name = context;
-	printf("Started coroutine %s\n", name);
-	printf("%s: switch count %lld\n", name, coro_switch_count(this));
-	printf("%s: yield\n", name);
-	coro_yield();
+    while (data->shared_file_queue->file_ptr < data->shared_file_queue->file_count) {
+        int file_ptr = data->shared_file_queue->file_ptr;
+        data->shared_file_queue->file_ptr++;
 
-	printf("%s: switch count %lld\n", name, coro_switch_count(this));
-	printf("%s: yield\n", name);
-	coro_yield();
+        char *file_name = data->shared_file_queue->file_names[file_ptr];
+        int number_count = count_numbers_in_file(file_name);
+        int *numbers = get_numbers_in_file(file_name, number_count);
 
-	printf("%s: switch count %lld\n", name, coro_switch_count(this));
-	other_function(name, 1);
-	printf("%s: switch count after other function %lld\n", name,
-	       coro_switch_count(this));
-	free(name);
-	/* This will be returned from coro_status(). */
-	return 0;
+        printf("coroutine %s starts sorting file %s\n", data->name, file_name);
+        data->shared_file_queue->sorted_files[file_ptr] = get_sorted_inplace_file_data(number_count, numbers,
+                                                                                       &data->start_timespec,
+                                                                                       data->quantum);
+        printf("coroutine %s finishes sorting file %s\n", data->name, file_name);
+    }
+    printf("coroutine %s finished execution\n", data->name);
+
+    dispose_of_coroutine_data(coroutine_data);
+    return 0;
+}
+
+void output_merged_sorted_numbers_to_file(const struct file_queue *shared_file_queue, FILE *fp) {
+    printf("started merging\n");
+    int *file_ptr_to_current_number_ptr = malloc(sizeof(int) * shared_file_queue->file_count);
+    for (int file_ptr = 0; file_ptr < shared_file_queue->file_count; ++file_ptr)
+        file_ptr_to_current_number_ptr[file_ptr] = 0;
+    while (true) {
+        int *minimum_number = NULL;
+        int minimum_number_file_ptr = -1;
+        for (int file_ptr = 0; file_ptr < shared_file_queue->file_count; ++file_ptr) {
+            int current_number_ptr = file_ptr_to_current_number_ptr[file_ptr];
+            struct single_sorted_file_data *sorted_file = shared_file_queue->sorted_files[file_ptr];
+            int *current_number = &sorted_file->sorted_numbers[current_number_ptr];
+            if (sorted_file->number_count == current_number_ptr)
+                continue;
+            if (minimum_number == NULL || *minimum_number > *current_number) {
+                minimum_number_file_ptr = file_ptr;
+                minimum_number = current_number;
+            }
+        }
+        if (minimum_number == NULL)
+            break;
+        file_ptr_to_current_number_ptr[minimum_number_file_ptr]++;
+        fprintf(fp, "%d ", *minimum_number);
+    }
+    free(file_ptr_to_current_number_ptr);
+    printf("finished merging\n");
 }
 
 int
-main(int argc, char **argv)
-{
-	/* Delete these suppressions when start using the args. */
-	(void)argc;
-	(void)argv;
-	/* Initialize our coroutine global cooperative scheduler. */
-	coro_sched_init();
-	/* Start several coroutines. */
-	for (int i = 0; i < 3; ++i) {
-		/*
-		 * The coroutines can take any 'void *' interpretation of which
-		 * depends on what you want. Here as an example I give them
-		 * some names.
-		 */
-		char name[16];
-		sprintf(name, "coro_%d", i);
-		/*
-		 * I have to copy the name. Otherwise all the coroutines would
-		 * have the same name when they finally start.
-		 */
-		coro_new(coroutine_func_f, strdup(name));
-	}
-	/* Wait for all the coroutines to end. */
-	struct coro *c;
-	while ((c = coro_sched_wait()) != NULL) {
-		/*
-		 * Each 'wait' returns a finished coroutine with which you can
-		 * do anything you want. Like check its exit status, for
-		 * example. Don't forget to free the coroutine afterwards.
-		 */
-		printf("Finished %d\n", coro_status(c));
-		coro_delete(c);
-	}
-	/* All coroutines have finished. */
+main(int argc, char **argv) {
+    const int non_file_name_cli_arguments = 3;
 
-	/* IMPLEMENT MERGING OF THE SORTED ARRAYS HERE. */
+    // argv[0] is the executable
+    int target_latency;
+    sscanf(argv[1], "%i", &target_latency);
+    int coroutine_count;
+    sscanf(argv[2], "%i", &coroutine_count);
+    int quantum = target_latency / coroutine_count;
 
-	return 0;
+    struct file_queue *shared_file_queue = create_file_queue(
+            argc - non_file_name_cli_arguments,
+            argv + non_file_name_cli_arguments
+    );
+
+
+    /* Initialize our coroutine global cooperative scheduler. */
+    coro_sched_init();
+    /* Start several coroutines. */
+    for (int i = 0; i < coroutine_count; ++i) {
+        char name[16];
+        sprintf(name, "coro_%d", i);
+        struct coroutine_data *coroutine_data = create_coroutine_data(strdup(name), quantum, shared_file_queue);
+        coro_new(coroutine_func_f, coroutine_data);
+    }
+    /* Wait for all the coroutines to end. */
+    struct coro *c;
+    while ((c = coro_sched_wait()) != NULL) {
+        /*
+         * Each 'wait' returns a finished coroutine with which you can
+         * do anything you want. Like check its exit status, for
+         * example. Don't forget to free the coroutine afterwards.
+         */
+        printf("Finished %d\n", coro_status(c));
+        coro_delete(c);
+    }
+    /* All coroutines have finished. */
+
+    FILE *fp = fopen("goal.txt", "w");
+    output_merged_sorted_numbers_to_file(shared_file_queue, fp);
+    fclose(fp);
+
+    dispose_of_file_queue(shared_file_queue);
+
+    return 0;
 }
+
